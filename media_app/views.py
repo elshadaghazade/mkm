11a72a203e54cd7592e667259e72372e2933a43a
cavidanhasanli <cavidan.hasanli@mail.ru>
2019-01-16 12:44:28 +0400

    bugfix issue6

--- media_app/views.py
+++ media_app/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
-
+from company_app.models import *
+import math
 # Create your views here.
-def photo_gallery(request):
-    context = {}
-    return render(request, "photo_gallerry.html", context)
+def photo_gallery(request,pk):
+    context = {
+        'company': Companies.objects.get(pk=pk),
+        'company_id': pk,
+    }
+    
+    try:
+        start = int(request.GET.get('start', 0)) - 1
+    except:
+        start = 0
 
-def photo_gallery_detailed(request):
-    context = {}
+    limit = 100
+    company_count = CompanyPhotoGallery.objects.count()
+
+    if start < 0:
+        start = 0
+    elif start > math.ceil(company_count / limit):
+        start = math.ceil(company_count / limit) - 1
+
+    context['photo_list'] = CompanyPhotoGallery.objects.all()[start * limit:start * limit +limit]
+    context['horizontal_limit'] = 3
+
+    return render(request, "photo_gallery.html", context)
+
+def photo_gallery_detailed(request,pk,photo_gallery_id):
+    context = {
+        'company': Companies.objects.get(pk=pk),
+        'company_id': pk,
+        #'photo_id': photo_gallery_id,
+    }
+    
     return render(request, "photo_gallery_detailed.html", context)
     
 def video_gallery(request):
