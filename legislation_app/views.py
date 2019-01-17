75141886124dba921e2f627bde39b8ff9910d15f
cavidanhasanli <cavidan.hasanli@mail.ru>
2019-01-17 17:30:27 +0400

    bugfix_FAQ_and_legislation.html

--- legislation_app/views.py
+++ legislation_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from company_app.models import Companies
 
 # Create your views here.
-def legislation(request):
-    context = {}
+def legislation(request,pk):
+    context = {
+        'company': Companies.objects.get(pk=pk),
+        'company_id':pk,
+    }
     return render(request, "legislation.html", context)
--- vip_app/views.py
+++ vip_app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from company_app.models import Companies
 
 
 # Create your views here.
-def distinguished_ones(request):
-    context = {}
+def distinguished_ones(request,pk):
+    context = {
+        'company': Companies.objects.get(pk=pk),
+        'company_id': pk,
+    }
     return render(request, "distinguished_ones.html", context)
 
 
-def medalist(request):
-    context = {}
+def medalist(request,pk):
+    context = {
+        'company': Companies.objects.get(pk=pk),
+        'company_id': pk,
+    }
     return render(request, "medalist.html", context)
