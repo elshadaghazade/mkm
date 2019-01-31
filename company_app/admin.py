c346bcf7f4475e378739fd0532c5e7ca81ebfb34
Elshad Agayev <elshadaghazade@gmail.com>
2019-01-31 10:55:23 +0400

    commit

--- company_app/admin.py
+++ company_app/admin.py
@@ -2,9 +2,4 @@ from django.contrib import admin
 from company_app.models import *
 # Register your models here.
 
-admin.site.register(CompanyProfile)
-admin.site.register(Companies)
-admin.site.register(CompanyAdministration)
-admin.site.register(CompanyPhotoGallery)
-admin.site.register(CompanyVideoGallery)
-admin.site.register(CompanyInformativePages)
+admin.site.register([CompanyProfile, Companies, CompanyAdministration, CompanyPhotoGallery, CompanyVideoGallery,CompanyInformativePages])
